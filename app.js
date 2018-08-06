6e08c6f709e1cbb7c09fb0ec6715c982a582b0cb
Micah <mjayfinley@gmail.com>
2018-08-06 12:41:06 -0500

    heroku try four

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.delete('/product/:productId', (req,res) => {
 
 //after all my routes//
 if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, './lindas-shop-client/build')))
+app.use(express.static(path.join(__dirname, '../lindas-shop-client/build')))
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
