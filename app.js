abc7fe9d524a3a7b2327df5a12555e8337d31d10
Micah <mjayfinley@gmail.com>
2018-08-07 11:14:11 -0500

    .

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(bodyParser.json())
 
-app.use(function(req,res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+// app.use(function(req,res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// })
 
 
 
